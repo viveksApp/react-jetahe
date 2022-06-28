e6d9cbc240181300786048396cbd04caf42cdea8
viveksApp <viveksalve05@gmail.com>
2022-06-28 11:44:33 +0530

    2 files modified

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,14 +7,27 @@ var ProgressBar = window.ReactProgressBarPlus;
 
 class About extends React.Component {
   componentDidMount() {
-    this.props.stopLoading();
+    //this.props.stopLoading();
+
+    axios({
+      url: 'https://jsonplaceholder.typicode.com/posts',
+      delay: 30000, // Post executation delay
+    }).then((response) => {
+      this.setState({
+        isLoaded: true,
+        items: response.data,
+        pageTitle: 'Welcome Here',
+      });
+      console.log(response.data);
+      this.props.stopLoading();
+    });
   }
 
   render() {
     return (
       <>
         <Helmet>
-          ‍<title />
+          ‍ ‍<title>Datatable</title>
         </Helmet>
         <br />
         <p>This is about us</p>
